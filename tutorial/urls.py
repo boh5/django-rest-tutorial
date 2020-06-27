ec0348ba1ea1e18b0cf09a037938ef18a002315b
dilless <bohuang1994@gmail.com>
2020-06-27 23:14:06 +0800

    jwt

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', include('snippets.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
