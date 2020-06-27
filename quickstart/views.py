3c152c1198fd5f7ef85a2fcd363ab7690ce37179
dilless <bohuang1994@gmail.com>
2020-06-27 14:47:01 +0800

    quickstart

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
 
 # Create your views here.
+from quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    用户 API
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    用户组 API
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
